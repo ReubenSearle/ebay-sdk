c4a2b0312c35b63ff5923f9cedb4661f0a3a9e6b
Reuben Searle <reubensearle@googlemail.com>
2021-01-20 13:10:23 +0000

    Include index for enums

---
+++ lib/esm/enums/index.d.ts
@@ -0,0 +1,10 @@
+import * as marketplaceCategories from './category';
+import condition from './condition';
+import currency from './currency';
+import distanceUnits from './distanceUnits';
+import itemFilterType from './itemFilterType';
+import itemSortOrder from './itemSortOrder';
+import listingType from './listingType';
+import marketplaceId from './marketplaceId';
+import shippingType from './shippingType';
+export { marketplaceCategories, condition, currency, distanceUnits, itemFilterType, itemSortOrder, listingType, marketplaceId, shippingType };
