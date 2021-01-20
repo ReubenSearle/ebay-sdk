0ffa20afc8733ddbc33d60e16103bc8d5401e2d4
Reuben Searle <reubensearle@googlemail.com>
2021-01-20 11:12:49 +0000

    Include condition enum

---
+++ lib/esm/enums/condition.d.ts
@@ -0,0 +1,14 @@
+declare enum condition {
+    acceptable = 6000,
+    forPartsOrNotWorking = 7000,
+    good = 5000,
+    likeNew = 2750,
+    new = 1000,
+    manufacturerRefurbishedOrCertifiedRefurbished = 2000,
+    newOther = 1500,
+    newWithDefects = 1750,
+    sellerRefurbished = 2500,
+    used = 3000,
+    veryGood = 4000
+}
+export default condition;
